10ce3d2a9732a755fd9e6dd5c94742b67d066d02
genspark_dev <genspark_dev@genspark.ai>
2025-11-12 02:05:47 +0000

    Feat: 지원자 목록 UI 개선 - 개인정보 섹션 추가 및 SNS 링크 클릭 가능하게 변경

--- src/index.ts
+++ src/index.ts
@@ -73,9 +73,9 @@ app.get('/', (c) => {
         <script src="https://cdn.jsdelivr.net/npm/flatpickr/dist/l10n/ko.js"></script>
         <!-- Daum 우편번호 API -->
         <script src="//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js"></script>
-        <script src="/static/js/api.js?v=4"></script>
-        <script src="/static/js/ui-utils.js?v=4"></script>
-        <script src="/static/js/app.js?v=4"></script>
+        <script src="/static/js/api.js?v=5"></script>
+        <script src="/static/js/ui-utils.js?v=5"></script>
+        <script src="/static/js/app.js?v=5"></script>
     </body>
     </html>
   `);
